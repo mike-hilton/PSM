c5a8f4ef6d98b64a828d764141c681cc8d5cfa39
Mike <lovethembytes@gmail.com>
2017-12-31 23:21:25 +0100

    Changed variable names

--- examples/example.py
+++ examples/example.py
@@ -11,18 +11,18 @@ msg = you.publickey_encrypt_message([me.id], "My secret message encrypted with m
 print
 print "Result after encryption with private key:\n%s" % msg
 
-dec_me = me.decrypt_message(msg)
+dec_msg = me.decrypt_message(msg)
 print
-print "Result after decryption with private key:\n%s" % dec_me
+print "Result after decryption with private key:\n%s" % dec_msg
 
 me.add_sharedKey(label="client")
 msg2 = me.secretkey_encrypt_message("My secret message encrypted with a shared key", "client")
 print
 print "Result after encryption with shared key:\n%s" % msg2
 
-dec2 = me.decrypt_message(msg2)
+dec_msg2 = me.decrypt_message(msg2)
 print 
-print "Result after decryption with shared key:\n%s" % dec2
+print "Result after decryption with shared key:\n%s" % dec_msg2
 
 print
 print "Shared key with label 'client':\n%s" % me.get_sharedKey("client")
